ec78ddf69e7efd34f31f9a139541ca2892f04b86
Kobuntu <odon0188@gmail.com>
2020-06-03 11:54:51 +0300

    Create paginator-resources.js

---
+++ src/i18n/RU/paginator-resources.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Spanish resource strings for IgxGrid
+ */
+exports.PaginatorResourceStringsRU = {
+    igx_paginator_label: 'Записей на странице',
+    igx_paginator_pager_text: 'из'
+};
